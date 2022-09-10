6e926b7adefd372eb79d96266b0e24cd2edf5f04
MIshmail <michaely.ishmail@gmail.com>
2022-09-09 21:59:26 -0400

    added styling and navigation

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// IMPORT COVER LETTER BELOW
+//import CV from '../../assets/'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href='' className='btn'>Download CV</a>
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+
+// IMPORT YOUR IMAGE
+//import ME from '../../assets'
+
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Michael Ishmail</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src="" alt="" />
+        </div>
+
+        <a href='#contact' className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
